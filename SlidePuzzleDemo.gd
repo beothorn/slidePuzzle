c08cf03bfe4d62cf93642b33145d1aa9ee148708
lucass <lucass@gmail.com>
2020-08-31 23:44:50 +0200

    Fixed code snippet, hide next button on last slide

--- SlidePuzzleDemo.gd
+++ SlidePuzzleDemo.gd
@@ -1,20 +1,30 @@
 extends Node2D
 
+var can_move: bool = true
+const slide_count = 8
+const slide_size = 1024
+
 func _on_puzzle_solved(puzzle_name: String) -> void:
 	$CanvasLayer/PuzzleSolved.visible = true
 	$CanvasLayer/PuzzleSolved/Player.play("Solved")
 
 func _on_nextSlide_button_down():
-	var new_x_position = $Background.position.x + 1024
-	$CanvasLayer/previousSlide.visible = new_x_position != 0
-	$Background.move(new_x_position)
-	$CanvasLayer/PuzzleSolved.visible = false
+	if can_move:
+		can_move = false
+		var new_x_position = $Background.position.x + slide_size
+		$CanvasLayer/previousSlide.visible = new_x_position != 0
+		$CanvasLayer/nextSlide.visible = new_x_position < slide_size * (slide_count-1)
+		$Background.move(new_x_position)
+		$CanvasLayer/PuzzleSolved.visible = false
 
 func _on_previousSlide_button_down():
-	var new_x_position = $Background.position.x - 1024
-	$CanvasLayer/previousSlide.visible = new_x_position != 0
-	$Background.move(new_x_position)
-	$CanvasLayer/PuzzleSolved.visible = false
+	if can_move:
+		can_move = false
+		var new_x_position = $Background.position.x - slide_size
+		$CanvasLayer/previousSlide.visible = new_x_position != 0
+		$CanvasLayer/nextSlide.visible = new_x_position < slide_size * (slide_count-1)
+		$Background.move(new_x_position)
+		$CanvasLayer/PuzzleSolved.visible = false
 
 
 func _on_SlidePuzzle_clicked_tile(pos):
@@ -39,3 +49,7 @@ func _on_SlidePuzzle_piece_moved(piece, tile_from, tile_to):
 func _on_SlidePuzzle_piece_released(piece, tile):
 	#print("Released: Piece "+piece.name+" at "+str(tile))
 	pass
+
+
+func _on_Tween_tween_all_completed():
+	can_move = true
